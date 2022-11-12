340c4b4323f6578809a55bcc9e40b6fec3a60364
singhtushar689 <singhtushar689@gmail.com>
2022-11-12 23:32:46 +0530

    Table of Power of 2

--- Program.cs
+++ Program.cs
@@ -4,30 +4,38 @@ namespace BasicCoreProgramming
 {
     internal class Program
     {
-         
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To the Leap Year Program");
-            Console.WriteLine("Enter the Four -Digit Year");
-            double Year = Convert.ToInt32(Console.ReadLine());
-            if (Year > 999)
+            Console.WriteLine("Welcome To the Power of 2 Program");
+            Console.WriteLine("Enter the Power Of 2");
+            int N =Convert.ToInt32(Console.ReadLine());
+            if (N > 0 && N < 31)
             {
-                if ((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0))
+                for (int i = 1; i <= N; i++)
                 {
-                    Console.WriteLine("It is a leap year");
-                }
-                else
-                {
-                    Console.WriteLine("It is not a leap Year");
+                    double number = Math.Pow(2, i);
+                    Console.WriteLine(number);
                 }
             }
+            
             else
-                Console.WriteLine("Invalid Entry");
-    
+            {
+                Console.WriteLine("Invalid Number");
+            }
+             
+
+            
+         
+
+        
 
 
 
-        }
+
+
+
+        } 
     }
 }
