2b7d3e9986a9070ad6bc77c2e376f380e97624dd
singhtushar689 <singhtushar689@gmail.com>
2022-11-12 22:57:14 +0530

    Check Leap Year

--- Program.cs
+++ Program.cs
@@ -8,41 +8,23 @@ namespace BasicCoreProgramming
 
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Welcome to the Flip Coin Problem");
-            int Head = 0;
-            int Tails = 0;
-            int Coin = 0;
-            const int MaxFlip = 10;
-            double HeadPercentage = 0;
-            double TailsPercentage = 0;
-           
-           
-
-            for (Coin = 1; Coin <= 10 ;Coin ++ )
+            Console.WriteLine("Welcome To the Leap Year Program");
+            Console.WriteLine("Enter the Four -Digit Year");
+            double Year = Convert.ToInt32(Console.ReadLine());
+            if (Year > 999)
             {
-                Random random = new Random();
-                 int CoinCheck = random.Next(0, 2);
-
-                if (CoinCheck <= 0.5)
+                if ((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0))
                 {
-                    Console.WriteLine("Head");
-                    Head++;
+                    Console.WriteLine("It is a leap year");
                 }
                 else
                 {
-                    Console.WriteLine("Tail");
-                    Tails++;
+                    Console.WriteLine("It is not a leap Year");
                 }
             }
-            Console.WriteLine("{0} out of {1} " ,Head,10);
-            Console.WriteLine("{0} out of {1} " ,Tails,10);
-
-            HeadPercentage = ((Head *100)/ MaxFlip);
-            Console.WriteLine("Head Percentage is {0}% ", HeadPercentage);
-            
-            TailsPercentage = ((Tails * 100) / MaxFlip);
-            Console.WriteLine( "TailsPercentage is {0}% ",TailsPercentage);
+            else
+                Console.WriteLine("Invalid Entry");
+    
 
 
 
