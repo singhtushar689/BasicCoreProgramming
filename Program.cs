ccbf534d8ae19eefd79558f98bd14b8ead81fc1d
singhtushar689 <singhtushar689@gmail.com>
2022-11-15 16:10:35 +0530

    Quotient & Remainder

--- Program.cs
+++ Program.cs
@@ -8,37 +8,16 @@ namespace BasicCoreProgramming
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Harmonic Number Program");
-            Console.WriteLine("Enter the Value of N");
-            int N =Convert.ToInt32(Console.ReadLine());
-            double Sum = 0;
-            if (N > 0 )
-            {
-                for (double i = 1; i <= N; i++)
-                {
-                    double HarmonicNumber = (1 / i);
-                    Sum += HarmonicNumber;
-                    Console.WriteLine("The Harmonic Number is " + HarmonicNumber.ToString());
-                }
-                Console.WriteLine("The Sum of Harmonic Number is " + Sum);
-            }
+            Console.WriteLine("Welcome To Quotient and Remainder Program");
+            Console.WriteLine("Enter the Value of Dividend");
+            int Dividend =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Value of Divisor");
+            int Divisor = Convert.ToInt32(Console.ReadLine());
+
+            long Calculation = Math.DivRem(Dividend, Divisor , out long result);
+            Console.WriteLine("Quotient is" + Calculation);
+            Console.WriteLine("Remainder is" + result);
             
-            else
-            {
-                Console.WriteLine("Invalid Entry");
-            }
-             
-
-            
-         
-
-        
-
-
-
-
-
-
         } 
     }
 }
